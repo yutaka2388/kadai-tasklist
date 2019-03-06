cd0967721a1302262e5c8509efb1219d7184105a
EC2 Default User <ec2-user@ip-172-31-77-53.ec2.internal>
2019-03-06 11:42:47 +0000

    kadai_re

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   root to: 'tasks#index'
 
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+
   resources :tasks
+  resources :relationships, only: [:create, :destroy]
+  
 end
